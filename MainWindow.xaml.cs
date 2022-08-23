f19e9bf1f4567f03e5a54539715c63213571a716
thea Villanueva <tei.villanueva@gmail.com>
2022-08-23 00:30:37 -0700

    Create operation methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace CalculatorAppWPF
     public partial class MainWindow : Window
     {
         double lastNumber, result;
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,22 +35,39 @@ namespace CalculatorAppWPF
 
         private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
+            double newNumber;
+            if (double.TryParse((string)resultLabel.Content, out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        break;
+                    case SelectedOperator.Subtraction:
+                        break;
+                    case SelectedOperator.Multiplication:
+                        break;
+                    case SelectedOperator.Division:
+                        break;
+                    default:
 
+                        break;
+                }
+            }
         }
 
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
         {
+            // tryparse will evaluate whether its valid entry
             if (double.TryParse((string)resultLabel.Content, out lastNumber))
             {
                 lastNumber = lastNumber / 100;
                 resultLabel.Content = lastNumber.ToString();
             }
- 
- 
         }
 
         private void NegativeButton_Click(object sender, RoutedEventArgs e)
         {
+            
             // needs to convert from object to string to double, if its true, value will be stored in lastNumber
             if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
             {
@@ -58,9 +76,77 @@ namespace CalculatorAppWPF
             }
         }
 
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == multiplicationButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divisionButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == subtractionButton)
+                selectedOperator = SelectedOperator.Subtraction;
+            if (sender == additionButton)
+                selectedOperator = SelectedOperator.Addition;
+            
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            //sender carries the information that triggers the event handlers
+
+            //refactoring if statements
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
+            
+            /*int selectedValue = 0;
+
+            //sender carries the information that triggers the event handlers
+            if (sender == zeroButton)
+                selectedValue = 0;
+            if (sender == oneButton)
+                selectedValue = 1;
+            if (sender == twoButton)
+                selectedValue = 2;
+            if (sender == threeButton)
+                selectedValue = 3;
+            if (sender == fourButton)
+                selectedValue = 4;
+            if (sender == fiveButton)
+                selectedValue = 5;
+            if (sender == sixButton)
+                selectedValue = 6;
+            if (sender == sevenButton)
+                selectedValue = 7;
+            if (sender == eightButton)
+                selectedValue = 8;
+            if (sender == nineButton)
+                selectedValue = 9;*/
+
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}"; 
+            }
+        }
+
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
         }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
     }
 }
